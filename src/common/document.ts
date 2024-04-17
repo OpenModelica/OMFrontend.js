82c53e09a50f298bb26db9ff98f0757d718f1330
Evan Hedbor <evan@hedbor.org>
2024-04-17 17:34:17 +0200

    Update tree-sitter and web-tree-sitter (#12)
    * bump tree-sitter version
    
    * Update tree-sitter (v0.21.1), web-tree-sitter (0.22.5)

--- src/common/document.ts
+++ src/common/document.ts
@@ -27,15 +27,15 @@
  import { ModelicaScope } from "./scope.js";
  import { ModelicaClassSymbol, ModelicaNamedElementSymbol } from "./symbols.js";
  import { ModelicaComponentReferenceExpressionSyntax, ModelicaIdentifierSyntax, ModelicaNameSyntax, ModelicaStoredDefinitionSyntax, ModelicaTypeSpecifierSyntax } from "./syntax.js";
- 
+
  export class ModelicaDocument implements ModelicaScope, TextDocument {
- 
+
      #context: ModelicaContext;
      #document: TextDocument;
      #tree: Tree;
      #symbols: ModelicaClassSymbol[];
      #syntax: ModelicaStoredDefinitionSyntax;
- 
+
      constructor(context: ModelicaContext, uri: string, version: number, content: string) {
          this.#context = context;
          this.#document = TextDocument.create(uri, "modelica", version, content);
@@ -43,88 +43,88 @@
          this.#syntax = new ModelicaStoredDefinitionSyntax(this.#tree.rootNode);
          this.#symbols = [];
      }
- 
+
      private asPoint(position: Position): Point {
          return {
              column: position.character,
              row: position.line
          };
      }
- 
+
      get context(): ModelicaContext {
          return this.#context;
      }
- 
+
      getText(range?: Range): string {
          return this.#document.getText(range);
      }
- 
+
      get languageId(): string {
          return this.#document.languageId;
      }
- 
+
      get lineCount(): number {
          return this.#document.lineCount;
      }
- 
+
      offsetAt(position: Position): number {
          return this.#document.offsetAt(position);
      }
- 
+
      positionAt(offset: number): Position {
          return this.#document.positionAt(offset);
      }
- 
+
      async resolve(reference: ModelicaIdentifierSyntax | ModelicaNameSyntax | ModelicaTypeSpecifierSyntax | string[] | string | null | undefined, global?: boolean): Promise<ModelicaNamedElementSymbol | null> {
          throw new Error("Method not implemented.");
      }
- 
+
      async resolveFunction(reference: ModelicaComponentReferenceExpressionSyntax | ModelicaIdentifierSyntax | ModelicaNameSyntax | ModelicaTypeSpecifierSyntax | string[] | string | null | undefined, global?: boolean): Promise<ModelicaClassSymbol | null> {
          return null;
      }
- 
+
      get syntax(): ModelicaStoredDefinitionSyntax {
          return this.#syntax;
      }
- 
+
      * syntaxErrors(node?: SyntaxNode): IterableIterator<SyntaxNode> {
- 
+
          if (node == null)
              node = this.tree.rootNode;
- 
-         if (!node.hasError())
+
+         if (!node.hasError)
              return;
- 
-         if (node.type === "ERROR" || node.isMissing())
+
+         if (node.type === "ERROR" || node.isMissing)
              yield node;
- 
+
          for (let child of node.children)
              yield* this.syntaxErrors(child);
- 
+
      }
- 
+
      get tree(): Tree {
          return this.#tree;
      }
- 
+
      update(text: string, range?: Range): void {
- 
+
          if (range == null) {
- 
+
              TextDocument.update(this.#document, [{ text: text }], this.#document.version + 1);
              this.#tree = this.#context.parse(this.#document.getText());
- 
+
          } else {
- 
+
              let startIndex = this.offsetAt(range.start);
              let oldEndIndex = this.offsetAt(range.end);
              let startPosition = this.asPoint(this.positionAt(startIndex));
              let oldEndPosition = this.asPoint(this.positionAt(oldEndIndex));
              let newEndIndex = startIndex + text.length;
- 
+
              TextDocument.update(this.#document, [{ range: range, text: text }], this.#document.version + 1);
              let newEndPosition = this.asPoint(this.positionAt(newEndIndex));
- 
+
              this.#tree.edit({
                  newEndIndex: newEndIndex,
                  newEndPosition: newEndPosition,
@@ -133,9 +133,9 @@
                  startIndex: startIndex,
                  startPosition: startPosition
              });
- 
+
              this.#tree = this.#context.parse((index: number, position?: Point) => {
- 
+
                  if (position != null) {
                      return this.getText({
                          end: {
@@ -148,27 +148,27 @@
                          }
                      });
                  }
- 
+
                  return this.getText({
                      end: this.positionAt(index + 1),
                      start: this.positionAt(index)
                  });
- 
+
              }, this.#tree);
- 
+
          }
- 
+
          this.#syntax = new ModelicaStoredDefinitionSyntax(this.#tree.rootNode);
- 
+
      }
- 
+
      get uri(): string {
          return this.#document.uri;
      }
- 
+
      get version(): number {
          return this.#document.version;
      }
- 
+
  }
  
